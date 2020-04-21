856b73838464b97286e3b491264cdbda983c4761
lalit-vasoya <lalitvasoya.286@gmail.com>
2020-04-21 15:30:25 +0530

    Create Model

--- foodfiesta/foodfiesta/settings.py
+++ foodfiesta/foodfiesta/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'restaurantview.apps.RestaurantviewConfig',
+    'accounts.apps.AccountsConfig',
+    'adminview.apps.AdminviewConfig',
+    'cart.apps.CartConfig',
+    'cardview.apps.CardviewConfig',
 ]
 
 MIDDLEWARE = [
