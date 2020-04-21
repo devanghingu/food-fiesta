4daf28478720dd6cff0a8431b63f8b8b957b9a93
devanghingu <43535635+devanghingu@users.noreply.github.com>
2020-04-21 17:32:22 +0530

    models added

--- foodfiesta/foodfiesta/urls.py
+++ foodfiesta/foodfiesta/urls.py
@@ -5,8 +5,12 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('',include('userrestaurnt.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ foodfiesta/userrestaurnt/constants.py
@@ -0,0 +1,12 @@
+
+
+PANDING     =   1
+ACCEPTED    =   2
+REJECTED    =   3
+PREPARED    =   4
+DELIVERED   =   5
+STATUS=((PANDING:'Pending'),
+        (ACCEPTED:'Accepted'),
+        (REJECTED:'Rejected'),
+        (PREPARED:'Prepared'),
+        (DELIVERED:'Delivered'))
