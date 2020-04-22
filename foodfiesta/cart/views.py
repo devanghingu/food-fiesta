61eb753cc0e45c1c86d1494bdb30ee3d9e09f8e7
devanghingu <43535635+devanghingu@users.noreply.github.com>
2020-04-22 10:15:48 +0530

    migration branch marged

--- foodfiesta/cart/views.py
+++ foodfiesta/cart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+class cart(View):
+    def get(self,request,*args, **kwargs):
+        return render(request,'frontend/user_cart.html')
+    def post(self,request,*args, **kwargs):
+        pass
+
+class restaurant(View):
+    def get(self,request,*args, **kwargs):
+        return render(request,'frontend/user_restaurant.html')
+
+    def post(self,request,*args, **kwargs):
+        pass
--- foodfiesta/foodfiesta/urls.py
+++ foodfiesta/foodfiesta/urls.py
@@ -8,9 +8,8 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('',include('userrestaurnt.urls')),
+    path('user/',include('cart.urls')),
     path('admin/', admin.site.urls),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
