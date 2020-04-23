015fe80916487e0602cc29759cc23633ec84d1c8
RishitAghera <rishit4879@gmail.com>
2020-04-23 14:24:41 +0530

    pagination

--- foodfiesta/cardview/views.py
+++ foodfiesta/cardview/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,11 +10,17 @@ from restaurantview.models import Restaurant
 class RestaurantList(View):
     def get(self, request, *args, **kwargs):
         res_list = Restaurant.objects.all()
+        paginator = Paginator(res_list, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         count=res_list.count()
-        return render(request, 'frontend/cardview/restaurantlist.html',{'res_list':res_list,'count':count})
+        return render(request, 'frontend/cardview/restaurantlist.html',{'page_obj':page_obj,'count':count})
 
     def post(self, request):
         searchinput = request.POST.get('inputcity').strip()
         res_list= Restaurant.objects.filter(city__name__icontains=searchinput)
+        paginator = Paginator(res_list, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         count=res_list.count()
         return render(request, 'frontend/cardview/restaurantlist.html', {'res_list': res_list,'count':count})
