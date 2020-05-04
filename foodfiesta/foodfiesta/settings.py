c0f867387a2d3b2f4a11c17f186a266c0ea44bd9
Kalpesh <kalpeshmakwna2610@gmail.com>
2020-05-04 11:22:49 +0530

    account registration error

--- foodfiesta/foodfiesta/settings.py
+++ foodfiesta/foodfiesta/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount', 
-    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
 
     'restaurantview.apps.RestaurantviewConfig',
